dec180dabba02924e94165280e66df6d78ce2665
surfshtt <fk8ghtt@gmail.com>
2025-05-13 11:01:34 +0300

    2.6
    fixed display bug in filters hub and added some new feautrures

--- EventHub/obj/Debug/net8.0/EventHub.AssemblyInfo.cs
+++ EventHub/obj/Debug/net8.0/EventHub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fa6c1aac18186d73deb6bddf41d112b24542790")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+826a1fdc842f55cffdf2ddacc75b416e7277f971")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

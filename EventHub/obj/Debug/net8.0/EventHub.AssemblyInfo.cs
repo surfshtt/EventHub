c69b1297cbc9178bfa86b608c94d31ceabae44f0
surfshtt <fk8ghtt@gmail.com>
2025-05-23 13:27:40 +0300

    2.9.1
    some redisign of reg/log page

--- EventHub/obj/Debug/net8.0/EventHub.AssemblyInfo.cs
+++ EventHub/obj/Debug/net8.0/EventHub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54a08cc926ccf508a35557eb2cb9368ed3156cf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b94a79a5e4fca88ed691aa799192224a9bb1fee")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

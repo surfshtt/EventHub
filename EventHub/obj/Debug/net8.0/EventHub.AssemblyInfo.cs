54a08cc926ccf508a35557eb2cb9368ed3156cf0
surfshtt <fk8ghtt@gmail.com>
2025-05-23 09:34:22 +0300

    2.8

--- EventHub/obj/Debug/net8.0/EventHub.AssemblyInfo.cs
+++ EventHub/obj/Debug/net8.0/EventHub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+000666893f53c1e108c156e253a4785764ba2d56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f279602364327c6f65852ac1a7b837fbd41c5b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
